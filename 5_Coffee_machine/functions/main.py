707c4edf520f602b94b90e8928837a7b10e04876
usmanov <ussarvar@gmail.com>
2022-03-21 22:49:21 +0500

    Changing main.py in cofee machine project

--- 5_Coffee_machine/functions/main.py
+++ 5_Coffee_machine/functions/main.py
@@ -30,23 +30,40 @@ resources = {
     "coffee": 100,
 }
 
+
 def order(drink):
     resources['water'] -= MENU[drink]["ingredients"]["water"]
     resources['milk'] -= MENU[drink]["ingredients"]["milk"]
     resources['coffee'] -= MENU[drink]["ingredients"]["coffee"]
+
 
 def report():
     print(f"Water: {resources['water']} ml\n"
           f"Milk: {resources['milk']} ml\n"
           f"Coffee: {resources['coffee']} g\n")
 
+
 def is_resources_sufficient(drink):
+    is_ok = 1
     if resources['water'] < MENU[drink]["ingredients"]["water"]:
         print("Sorry there is not enough water.")
+        is_ok = 0
     if resources['milk'] < MENU[drink]["ingredients"]["milk"]:
         print("Sorry there is not enough milk.")
+        is_ok = 0
     if resources['coffee'] < MENU[drink]["ingredients"]["coffee"]:
         print("Sorry there is not enough coffee.")
+        is_ok = 0
+    return is_ok
+
+
+def money_machine():
+    quarters = float(input("Quarters: "))
+    dimes = float(input("Dimes: "))
+    nickles = float(input("Nickles: "))
+    pennies = float(input("Pennies: "))
+    money = 0.25 * quarters + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
+    return money
 
 
 end = False
@@ -57,7 +74,15 @@ while not end:
     elif answer == 'report':
         report()
     else:
-        order(answer)
-        is_resources_sufficient(answer)
-
-
+        if is_resources_sufficient(answer) == 1:
+            if money_machine() < MENU[is_resources_sufficient(answer)]["cost"]:
+                print("Sorry that's not enough money. Money refunded.")
+            elif money_machine() >= MENU[is_resources_sufficient(answer)]["cost"]:
+                if money_machine() == MENU[is_resources_sufficient(answer)]["cost"]:
+                    order(answer)
+                    print(f"Here is your {MENU[is_resources_sufficient(answer)]} ☕️. Enjoy!")
+                else:
+                    change = money_machine() - MENU[is_resources_sufficient(answer)]["cost"]
+                    print(f"Here is ${change} dollars in change.")
+                    order(answer)
+                    print(f"Here is your {MENU[is_resources_sufficient(answer)]} ☕️. Enjoy!")
